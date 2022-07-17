3bc4d843fc6daacc19c93c8c79ace8cbaa500fa9
edekr <ekrotter@mailbox.org>
2022-07-17 15:51:11 +0200

    Changes error no such file or directory requirements.txt

--- src/build_source_code.sh
+++ src/build_source_code.sh
@@ -1,4 +1,12 @@
 mkdir ../infrastructure/build
 
+# build fetch function
 cd fetch_jobs_lambda
 zip -r ../../infrastructure/build/fetch_jobs_lambda.zip .
+cd .. 
+
+# build request layer
+cd ../request_layer
+pip install -r requirements.txt -t python/lib/python3.9/site-packages
+zip -r ../../infrastructure/build/requests_layer.zip .
+cd ..
