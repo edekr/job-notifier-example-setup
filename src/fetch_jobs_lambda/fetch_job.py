4f60a362f4cb6f47a4ee257dd254aba00a69a4c4
edekr <ekrotter@mailbox.org>
2022-07-17 15:33:51 +0200

    Init fetch job data

--- src/fetch_jobs_lambda/fetch_job.py
+++ src/fetch_jobs_lambda/fetch_job.py
@@ -1,2 +1,17 @@
+import requests
+
+JOB_API_URL = 'https://www.arbeitnow.com/api/job-board-api'
+
+def load_job_data():
+    print("start loading job data")
+    response = requests.get(JOB_API_URL)
+    return response.json()
+
 def handle(event, context):
-    print("Hello")
+    job_api_data = load_job_data()
+    print(job_api_data)
+    print("Map Data")
+    print("Save Data")
+
+if __name__ == "__main__":
+    handle({},{})
