5c08f44fdb353ecd4c6657d4d5799452eee1f369
edekr <ekrotter@mailbox.org>
2022-07-17 16:05:11 +0200

    Changes error no such file or directory requirements.txt 3

--- infrastructure/fetch_jobs_data.tf
+++ infrastructure/fetch_jobs_data.tf
@@ -9,4 +9,13 @@ resource "aws_lambda_function" "fetch_jobs_lambda" {
   source_code_hash = filebase64sha256("build/fetch_jobs_lambda.zip")
 
   runtime = "python3.9"
+
+  layers = [ aws_lambda_layer_version.requests_layer.arn ]
+}
+
+resource "aws_lambda_layer_version" "requests_layer" {
+  filename   = "build/requests_layer.zip"
+  layer_name = "requests_layer"
+
+  compatible_runtimes = ["python3.9"]
 }
