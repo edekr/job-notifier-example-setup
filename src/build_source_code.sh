932e0e46aab21d1d3cfe6cfadb5c2c11c6b1498c
edekr <ekrotter@mailbox.org>
2022-07-25 11:13:09 +0200

    Add notifier

--- src/build_source_code.sh
+++ src/build_source_code.sh
@@ -3,6 +3,11 @@ mkdir ../infrastructure/build
 # build fetch function
 cd fetch_jobs_lambda
 zip -r ../../infrastructure/build/fetch_jobs_lambda.zip .
+cd ..
+
+# build job notifier
+cd new_job_notifier
+zip -r ../../infrastructure/build/new_job_notifier.zip .
 cd .. 
 
 # build request layer
